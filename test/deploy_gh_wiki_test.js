d767e0d0773057a5b941d7dc3ba025d3221ba2ad
Taka Okunishi <okunishinishi@gmail.com>
2015-08-07 19:35:30 +0900

    Version incremented to 1.0.4

--- test/deploy_gh_wiki_test.js
+++ test/deploy_gh_wiki_test.js
@@ -5,8 +5,10 @@
 
 var deployGhWiki = require('../lib/deploy_gh_wiki.js');
 
-exports['Deploy gh wiki'] = function (test) {
-    test.ok(deployGhWiki);
-    test.done();
+exports['Deploy gh wiki with invalid url.'] = function (test) {
+    deployGhWiki('foo/bar/*.md', "foo/bar", function (err) {
+        test.ok(!!err);
+        test.done();
+    });
 };
 
